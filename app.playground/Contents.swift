482bd1e394872d60b79d4de0cb1b9e83223f15f4
dlepore3366 <dlepore3366@student.pps.net>
2024-05-28 10:40:49 -0700

    First Commit

---
+++ app.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+import Foundation
+
+var greeting = "Hello, playground"
+
+let name = "dominic"
+let sd = "d"
+
+var names = [
+        name,
+        sd
+]
+names.append("ssfd")
+
+let foo = "Foo"
+var foo2 = foo
+foo2 = "Foo 2"
+foo
+foo2
+
+let moreNames = [
+    "loo",
+    "yoo"
+]
+
+var copy = moreNames
+
+copy.append("baz")
+moreNames
+copy
+
+
+let oldArray = NSMutableArray(
+    array: [
+        "var1",
+        "var2"
+    ]
+)
+
+oldArray.add("varThree")
+var newArray = oldArray
+newArray.add("dee")
+oldArray
+newArray
+
+let someNames = NSMutableArray(
+    array: [
+        "var1",
+        "var2"
+    ]
+)
+
+func changeTheArray(_)

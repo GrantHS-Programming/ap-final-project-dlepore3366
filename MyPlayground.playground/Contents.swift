567f519341f4791b1098319e9332d102f4422cfa
dlepore3366 <dlepore3366@student.pps.net>
2024-05-30 11:41:07 -0700

    f

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,40 @@
 import Foundation
 
 let myAge = 22
-ley yourAge = 20
+let yourAge = 20
 
-if myAge > yourAge{
+if myAge > yourAge {
     "im older than you"
 } else if myAge < yourAge {
     "I'm younger than you"
 } else {
     "Oh hey, we are the same age"
 }
+
+let momAge = myAge + 30
+let doubleMyage = myAge * 2
+
+let result = 1 + 2
+
+//^^^^ binary infix
+
+
+let foo = !true
+//unary prefix^^^
+
+let name = Optional("aslfkjasflk")
+type(of: name)
+
+let UnaryPostix = name!
+
+type(of: UnaryPostix)
+//unary postfix
+
+
+
+let age = 300
+
+
+let message = age >= 18
+    ? "you are an adult"
+    : "You are not yet an ault"
